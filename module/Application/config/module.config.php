923ae23f3d0c4a26f776fc291043fec037f7fcb4
Matthew Weier O'Phinney <matthew@zend.com>
2012-10-31 14:47:48 -0500

    Moved view changes to PhlyPaste module

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -18,8 +18,6 @@ return array(
             'layout/layout'           => __DIR__ . '/../view/layout/layout.phtml',
             'error/404'               => __DIR__ . '/../view/error/404.phtml',
             'error/index'             => __DIR__ . '/../view/error/index.phtml',
-            'phly-paste/paste/form'   => __DIR__ . '/../view/paste/form.phtml',
-            'phly-paste/paste/view'   => __DIR__ . '/../view/paste/view.phtml',
         ),
         'template_path_stack' => array(
             __DIR__ . '/../view',
